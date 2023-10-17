23a3adb12108141d042e2fa7d4e8ad2179e98cd6
ClaudomiroSales <claudomiro.sales@gmail.com>
2023-10-17 13:41:20 -0300

    Testando caixaColorida
    Deletando a memória.

--- main.cpp
+++ main.cpp
@@ -4,16 +4,35 @@ using std::cout;
 
 #include "Caixa.h"
 #include "Caixa.cpp"
+#include "CaixaColorida.h"
+#include "CaixaColorida.cpp"
+
 
 int main(int argc, char **argv)
-{
-    Caixa *caixas[10];
+{   
+    const int NUMMAXCAIXAS = 2;
+    Caixa         *caixas[NUMMAXCAIXAS];
+    CaixaColorida *caixasColoridas[NUMMAXCAIXAS];
 
     caixas[ 0 ] = new Caixa;
     cout << *caixas[ 0 ];
 
     caixas[ 1 ] = new Caixa(2, 2);
     cout << *caixas[ 1 ];
+
+    caixasColoridas[ 0 ] = new CaixaColorida;
+    cout << *caixasColoridas[ 0 ];
+
+    caixasColoridas[ 1 ] = new CaixaColorida(2, 2, 1, WHITE);
+    cout << *caixasColoridas[ 1 ];
+
+
+
+    for(int i = 0; i < NUMMAXCAIXAS; i++)
+    {
+        delete caixas[ i ];
+        delete caixasColoridas[ i ];
+    }
 
 
     return 0;
